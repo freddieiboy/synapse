35395cf1302d505333ebb8971baf9deda1be1a1e
freddieiboy <freddieiboy@gmail.com>
2016-05-18 20:00:00 -0700

    height fixes

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,14 +4,16 @@ class Dashboard extends Component {
   render() {
     const styles = {
       Dashboard: {
-        backgroundColor: '#000',
+        backgroundColor: '#1C272C',
         height: '100%',
-        width: '100%'
+      },
+      square: {
+        backgroundColor: '#D8CA41'
       }
     }
     return (
       <div className="Dashboard" style={styles.Dashboard}>
-        <h1>Hello from React!!</h1>
+        <div className="square" style={styles.square}></div>
       </div>
     )
   }
